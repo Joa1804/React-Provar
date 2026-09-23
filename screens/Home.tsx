af5c86f7566ade24121ab6f33730e33836d8cb5c
João Emanoel <emanuel23pagani76@gmail.com>
2026-09-23 18:00:04 -0300

    Commit.02: Comecando com o Projeto

---
+++ screens/Home.tsx
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Home() {
+  return (
+    <View>
+      <Text>Welcome to the Home Screen!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({})
